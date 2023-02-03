5078e56b78f98d6d039e88968bf98c8144fabe3b
LionDev27 <leonzhou2b@gmail.com>
2023-02-03 19:40:14 +0100

    Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rb2D;
+    private SpriteRenderer _sR;
+    private Vector2 _moveVector;
+    private float _horizontal, _vertical;
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+        _sR = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        GetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInputs()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _moveVector = new Vector2(_horizontal, _vertical);
+        _moveVector.Normalize();
+    }
+
+    private void Move()
+    {
+        //Movemos al player con la velocidad del RigidBody.
+        _rb2D.velocity = _speed * Time.deltaTime * _moveVector;
+        //Flipeamos el sprite del jugador sólo cuando se mueva.
+        if (_horizontal != 0) TurnPlayer();
+    }
+
+    private void TurnPlayer()
+    {
+        _sR.flipX = _horizontal < 0;
+    }
+}
