4e68bb3fa92816c0e2d018accf7ec5bc0690482a
Adolfo Ingelmo <Lodur_norbel@hotmail.com>
2023-02-04 22:21:31 +0100

    Mas texto

--- Assets/Scripts/Dialogue/ConversationSystem.cs
+++ Assets/Scripts/Dialogue/ConversationSystem.cs
@@ -88,9 +88,15 @@ namespace Dialogue
                 "Si, claro", "No te lo recomiendo, tus chakras no son compatibles", 
                 "Algo totalmente normal", "Aquí seguro que conoces a alguien mejor");
             CreateQuestion("Tengo que estar en casa antes de las 12", 
-                "Los servidores se reinician a las una", "Los astros recomiendan esperar a mas tarde", 
+                "Los servidores se reinician a las una, aun tienes tiempo", "Los astros recomiendan esperar a mas tarde", 
+                "Tu madre me ha dicho que puedes estar un rato mas", "¿Y perderte la mejor parte de la fiesta?");
+            CreateQuestion("Es muy tarde para mi, me voy", 
+                "Los servidores se reinician a las una, aun tienes tiempo", "Los astros recomiendan esperar a mas tarde", 
                 "Tu madre me ha dicho que puedes estar un rato mas", "¿Y perderte la mejor parte de la fiesta?");
             CreateQuestion("Me aburro", 
+                "Juguemos al mario Kart", "Íbamos a empezar el debate sobre la naturaleza", 
+                "No te aburras", "Echemos una partida de beer pong");
+            CreateQuestion("La peor fiesta de la historia", 
                 "Juguemos al mario Kart", "Íbamos a empezar el debate sobre la naturaleza", 
                 "No te aburras", "Echemos una partida de beer pong");
             CreateQuestion("Echo de menos a mi gato", 
@@ -99,16 +105,26 @@ namespace Dialogue
             CreateQuestion("Tengo sueño", 
                 "Los héroes no duermen mientras haya un crimen por resolver", "Ahora te traigo café en grano para despertarte", 
                 "Échate una siesta en el sillón de la sala", "¿Pero tu duermes?");
+            CreateQuestion("Me vy a dormir", 
+                "Los héroes no duermen mientras haya un crimen por resolver", "Ahora te traigo café en grano para despertarte", 
+                "Échate una siesta en el sillón de la sala", "¿Pero tu duermes?");
             CreateQuestion("Mañana tengo que ir a una Game Jam temprano", 
                 "¿Qué tecnología vas a usar?", "¿Y cómo aplicas tus estudios en eso?",
                 "Háblame de tu proyecto", "¿Tu en una Game Jam?");
             CreateQuestion("Tengo que ir al hospital", 
                 "Aguanta, Keanu Reeves aguantaría", "Espera, tengo un par de remedios naturales", 
                 "Tengo una tirita, ¿te vale?", "No creo, estás en plena forma");
+            //10
+            CreateQuestion("Algo me ha sentado mal", 
+                "Aguanta, Keanu Reeves aguantaría", "Espera, tengo un par de remedios naturales", 
+                "Tengo una aspirina, ¿te vale?", "No creo, estás en plena forma");
             CreateQuestion("Me ha llamado mi madre", 
                 "Ha llamado a la mía y dice que te quedes", "Tu verdadera madre, ¿la madre naturaleza?", 
                 "Ya le llamarás", "Pero no le vas a hacer caso, ¿verdad?");
             CreateQuestion("Esto me parece un ritual satánico mas que una fiesta",
+                "A que sí, mola mucho ¿verdad?", "Es toda una experiencia extrasensorial",
+                "Si, lo es y no pasa nada", "No hay huevos a quedarte mas tiempo");
+            CreateQuestion("Mas que una fiesta parece un ritual satánico",
                 "A que sí, mola mucho ¿verdad?", "Es toda una experiencia extrasensorial",
                 "Si, lo es y no pasa nada", "No hay huevos a quedarte mas tiempo");
             CreateQuestion("Esta música es un poco tétrica", 
@@ -126,6 +142,28 @@ namespace Dialogue
             CreateQuestion("Ahora empieza mi turno de trabajo", 
                 "Teletrabajas, puedes empezar en otro momento", "No te dejes manipular por el capitalismo", 
                 "Tu jefe ha llamado y te da la noche libre", "Tu no trabajas");
+            CreateQuestion("Tengo que irme a trabajar", 
+                "Teletrabajas, puedes empezar en otro momento", "No te dejes manipular por el capitalismo", 
+                "¿A estas horas?", "Tú no trabajas");
+            //20
+            CreateQuestion("Huele raro", 
+                "No intentes disimular", "Estamos preparando la diversión", 
+                "Aquí tengo el ambientador", "Allí están mis pesas");
+            CreateQuestion("Me he olvidado la chaqueta arriba", 
+                "Esta de Lucky Star", "La llevas puesta", 
+                "Te la traigo", "Para qué, haz flexiones");
+            CreateQuestion("Esta fiesta es un muermo", 
+                "Mejor que el lol", "¡¡¡Fiesta!!!", 
+                "Cambio la música", "Pongo el Wii Fit");
+            CreateQuestion("Mi madre ya tiene listo el puchero", 
+                "Aquí hay palomitas", "Vamos a hervir otra cosa aquí abajo", 
+                "El de mi madre también. Baja ahora", "Te traigo un plato de arroz blanco");
+            CreateQuestion("Me llaman para cenar", 
+                "Aquí hay palomitas", "Vamos a hervir otra cosa aquí abajo", 
+                "El de mi madre también. Baja ahora", "Te traigo un plato de arroz blanco");
+            CreateQuestion("Voy a por un vaso agua", 
+                "¿Te lo traigo con gas o sin gas?", "Mejor e raigo un zumo de frutas", 
+                "Pero si hay cerveza", "Compré bebidas isotónicas");
         }
 
         private void CreateQuestion(string question, 
