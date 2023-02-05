88f017daa288aa4c319df64ef7e3e66ebe87ec85
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-02-05 11:47:15 +0100

    dd

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 public class MainMenuButtons : MonoBehaviour
 {
     public GameObject Instruction;
+    public GameObject story;
     public float waitTime = 3f;
 
     public void StartButton()
     {
         Instruction.SetActive(true);
+        story.SetActive(true);
+
         StartCoroutine(LoadGame());
     }
 
