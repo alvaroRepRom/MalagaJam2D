8c8ec755f41923044de2b8d111cf2c1099f27d34
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-02-05 11:25:02 +0100

    instructions

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    public GameObject Instruction;
+
     public void StartButton()
     {
-        SceneManager.LoadScene("Game");
+        Instruction.SetActive(true);
+        StartCoroutine(LoadGame());
     }
 
 
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    private IEnumerator LoadGame()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Game");
     }
 }
